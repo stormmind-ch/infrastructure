c63a8f67b78e4f9197c10e17a7e2d062a300f5c2
duke-j <damian.x.ueltschi@protonmail.com>
2025-05-23 18:33:35 +0200

    path

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -2,14 +2,14 @@
 
 
 
-docker build -t proxy-test ../../../new_infra/infrastructure/docker/nginx/Dockerfile
+docker build -t proxy-test ../../../new_infra/infrastructure/docker/nginx/
 docker tag proxy-test stormmind/proxy-test
 docker push stormmind/proxy-test
 
-docker build -t frontend-test ../../../new_frontend/frontend/Dockerfile
+docker build -t frontend-test ../../../new_frontend/frontend/
 docker tag frontend-test stormmind/frontend-test
 docker push stormmind/frontend-test
 
-docker build -t backend-test ../../../new_backend/backend/Dockerfile
+docker build -t backend-test ../../../new_backend/backend/
 docker tag backend-test stormmind/backend-test
 docker push stormmind/backend-test
