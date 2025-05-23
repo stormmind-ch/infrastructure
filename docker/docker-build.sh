0313d290492fb7403c88cd747f5f224c164ade93
duke-j <damian.x.ueltschi@protonmail.com>
2025-05-23 18:28:34 +0200

    naming

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -2,14 +2,14 @@
 
 
 
-docker build -t proxy_test ../../../new_infra/infrastructure/docker/nginx/Dockerfile
-docker tag proxy_test stormmind/proxy_test
-docker push stormmind/proxy_test
+docker build -t proxy-test ../../../new-infra/infrastructure/docker/nginx/Dockerfile
+docker tag proxy-test stormmind/proxy-test
+docker push stormmind/proxy-test
 
-docker build -t frontend_test ../../../new_frontend/frontend/Dockerfile
-docker tag frontend_test stormmind/frontend_test
-docker push stormmind/frontend_test
+docker build -t frontend-test ../../../new-frontend/frontend/Dockerfile
+docker tag frontend-test stormmind/frontend-test
+docker push stormmind/frontend-test
 
-docker build -t backend_test ../../../new_backend/backend/Dockerfile
-docker tag backend_test stormmind/backend_test
-docker push stormmind/backend_test
+docker build -t backend-test ../../../new-backend/backend/Dockerfile
+docker tag backend-test stormmind/backend-test
+docker push stormmind/backend-test
